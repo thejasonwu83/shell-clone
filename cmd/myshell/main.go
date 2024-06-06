1901660fdcf65277ab4e93e99315e155560bc43c
Jason Wu <jason@Jasons-MBP.lan>
2024-06-06 12:01:52 -0500

    added support for unrecognized commands

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,15 +4,17 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	// fmt.Println("Logs from your program will appear here!")
-
 	// Uncomment this block to pass the first stage
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	switch input {
+	default: // unrecognized command
+		fmt.Fprint(os.Stdout, strings.TrimSpace(input)+": command not found\n")
+	}
 }
